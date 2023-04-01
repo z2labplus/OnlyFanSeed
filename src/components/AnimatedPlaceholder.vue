cea484f2f8b8619f4b56d31ebba9924057e9d9d3
sj <858570636@qq.com>
2023-04-01 09:43:57 +0800

    complete app

---
+++ src/components/AnimatedPlaceholder.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="animate-pulse bg-gradient-to-r from-gray-100 ">
+      &nbsp;
+    </div>
+  </template>
+  
---
+++ src/views/CityView.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+      <Suspense>
+        <template #default>
+          <AsyncCityView />
+        </template>
+        <template #fallback>
+          <CityViewSkeleton />
+        </template>
+      </Suspense>
+    </div>
+  </template>
+  
+  <script setup>
+  import AsyncCityView from "../components/AsyncCityView.vue";
+  import CityViewSkeleton from "../components/CityViewSkeleton.vue";
+  </script>
+  
